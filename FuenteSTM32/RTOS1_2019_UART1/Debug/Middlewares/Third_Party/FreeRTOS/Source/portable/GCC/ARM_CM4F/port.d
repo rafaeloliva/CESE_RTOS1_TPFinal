b4f269cdb8b81cbf663fc7544c4c5a03933c9aa1
Rafael Oliva <roliva@uarg.unpa.edu.ar>
2019-04-11 19:11:57 -0300

    Agregado documento Hitos 1 y 2 y primer borrador en HAL

---
+++ FuenteSTM32/RTOS1_2019_UART1/Debug/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.d
@@ -0,0 +1,29 @@
+Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.o: \
+ ../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c \
+ C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Inc/FreeRTOSConfig.h \
+ C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+
+C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+
+C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Inc/FreeRTOSConfig.h:
+
+C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+
+C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+
+C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
---
+++ FuenteSTM32/RTOS1_2019_UART1/Debug/Src/subdir.mk
@@ -0,0 +1,52 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/freertos.c \
+../Src/gpio.c \
+../Src/i2c.c \
+../Src/main.c \
+../Src/stm32f4xx_hal_msp.c \
+../Src/stm32f4xx_hal_timebase_tim.c \
+../Src/stm32f4xx_it.c \
+../Src/syscalls.c \
+../Src/system_stm32f4xx.c \
+../Src/usart.c 
+
+OBJS += \
+./Src/freertos.o \
+./Src/gpio.o \
+./Src/i2c.o \
+./Src/main.o \
+./Src/stm32f4xx_hal_msp.o \
+./Src/stm32f4xx_hal_timebase_tim.o \
+./Src/stm32f4xx_it.o \
+./Src/syscalls.o \
+./Src/system_stm32f4xx.o \
+./Src/usart.o 
+
+C_DEPS += \
+./Src/freertos.d \
+./Src/gpio.d \
+./Src/i2c.d \
+./Src/main.d \
+./Src/stm32f4xx_hal_msp.d \
+./Src/stm32f4xx_hal_timebase_tim.d \
+./Src/stm32f4xx_it.d \
+./Src/syscalls.d \
+./Src/system_stm32f4xx.d \
+./Src/usart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F411xE -I"C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Inc" -I"C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Drivers/STM32F4xx_HAL_Driver/Inc" -I"C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/include" -I"C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS" -I"C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F" -I"C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"C:/Work_EmbdSTM32/2018/CL3/WSAC6/RTOS1_2019_UART1/Drivers/CMSIS/Include"  -O3 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
